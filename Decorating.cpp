b5074765920e925fb9d2603650e18b2812059916
TiestoDJ007 <jackxiao007@outlook.com>
2019-03-10 20:58:49 +0800

    2019.03.10

---
+++ Decorating.cpp
@@ -0,0 +1,15 @@
+//
+// Created by a on 2019/3/10.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    short int i;
+    short unsigned int j;
+    j = 50000;
+    i = j;
+    cout << i << "" << j;
+    return 0;
+}
