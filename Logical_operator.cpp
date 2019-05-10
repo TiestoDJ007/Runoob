b52e2be833e4d8f649c8dde06621525ff4f1492a
TiestoDJ007 <jackxiao007@outlook.com>
2019-05-10 18:21:09 +0800

    2019.05.10

--- Logical_operator.cpp
+++ Logical_operator.cpp
@@ -1,4 +1,17 @@
 ﻿//
 // Created by a on 2019/5/9.
 //
+#pragma execution_character_set("utf-8")
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 20;
+    int c;
+    if (a && b) {
+        cout << "Line 1 - 条件为真" << endl;
+    }
+}
